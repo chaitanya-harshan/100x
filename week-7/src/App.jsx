2dd608e0f5f1b17206ffd15ad5dea9ef391fad48
chaitanya <chaitanyaharshan.official@gmail.com>
2025-01-18 19:09:08 +0530

    re-added week-7

---
+++ week-7/src/App.jsx
@@ -0,0 +1,73 @@
+import { useContext, useMemo, useState } from "react"
+import { CountContext } from "./context";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  )
+}
+
+function Count() {
+  console.log("count re-render");
+  return <div>
+    <CountRenderer />
+    <Buttons />
+    <EvenCountRenderer />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  
+  return <div>
+    <b>
+      {count}
+    </b>
+  </div>
+}
+
+// function EvenCountRenderer() {
+//   const count = useRecoilValue(countAtom);
+//   const isEven = useMemo(() => count % 2 === 0, [count]);
+  
+//   return <div>{isEven ? "It is even" : null}</div>
+// }
+
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+  return <div>
+    {isEven ? null : "It is even"} 
+    {/* 0 -> false, 1 -> true */}
+  </div>
+}
+
+// function EvenCountRenderer() {
+//   const isEven = useRecoilValue(evenSelector);
+
+//   return <div>
+//     {isEven ? "It is even" : null}
+//   </div>
+// }
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  console.log("buttons re-rendererd");
+
+  return <div>
+    <button onClick={() => {
+      setCount(count => count + 1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count => count - 1)
+    }}>Decrease</button>
+  </div>
+}
+
+export default App
---
+++ week-7/src/store/atoms/count.js
@@ -0,0 +1,24 @@
+// import { createContext, useMemo } from "react";
+import { atom, selector } from "recoil";
+
+export const countAtom = atom({
+    key: "countAtom",
+    default: 0
+});
+
+export const evenSelector = selector({
+    key: "evenSelector",
+    get: ({get}) => get(countAtom) % 2
+});
+
+
+// export const evenSelector = selector({
+//     key: "evenSelector",
+//     get: ({get}) => {
+//         const count = get(countAtom);
+//         return count % 2;
+//     }
+// });
+
+// Todo creation application with filtering logic
+// todos, filter
