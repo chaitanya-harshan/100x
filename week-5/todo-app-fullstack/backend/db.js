d34907a68f4a56a214ed9f0f3765d4d261333f7c
chaitanya <chaitanyaharshan.official@gmail.com>
2025-01-19 16:46:42 +0530

    changed all the modules to es6

--- week-5/todo-app-fullstack/backend/db.js
+++ week-5/todo-app-fullstack/backend/db.js
@@ -1,16 +1,10 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose'
 
-// mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos
-// .env
-mongoose.connect("mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos")
+mongoose.connect("mongodb+srv://lemon:chaispaceX1415@cluster0.3w06p3p.mongodb.net/todos-server")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
     completed: Boolean
 })
 
-const todo = mongoose.model('todos', todoSchema);
-
-module.exports = {
-    todo
-}
+export const todo = mongoose.model('todos', todoSchema);
--- week-5/todo-app-fullstack/backend/index.js
+++ week-5/todo-app-fullstack/backend/index.js
@@ -4,60 +4,49 @@ import { createTodo, updateTodo } from "./types";
 import { todo } from "./db";
 
 const app = express();
+app.listen(3000);
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/todo", async function(req, res) {
-    const createPayload = req.body;
-    const parsedPayload = createTodo.safeParse(createPayload);
+app.post("/todo", async (req, res) => {
+  const payload = req.body;
+  const parsedPayload = createTodo.safeParse(payload);
 
-    if (!parsedPayload.success) {
-        res.status(411).json({
-            msg: "You sent the wrong inputs",
-        })
-        return;
-    }
-    // put it in mongodb
-    await todo.create({
-        title: createPayload.title,
-        description: createPayload.description,
-        completed: false
-    })
+  if (!parsedPayload.success) {
+    return res.status(411).json({ msg: "You sent the wrong inputs" });
+  }
+  // put it in mongodb
+  await todo.create({
+    title: payload.title,
+    description: payload.description,
+    completed: false,
+  });
 
-    res.json({
-        msg: "Todo created"
-    })
-})
+  res.json({ msg: "Todo created" });
+});
 
-app.get("/todos", async function(req, res) {
-    // const todos = await todo.find({});
+app.get("/todos", async (req, res) => {
+  // const todos = await todo.find({});
 
-    res.json({
-        todos: []
-    })
+  res.json({
+    todos: [],
+  });
+});
 
-})
+app.put("/completed", async (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    return res.status(411).json({ msg: "You sent the wrong inputs" });
+  }
 
-app.put("/completed", async function(req, res) {
-    const updatePayload = req.body;
-    const parsedPayload = updateTodo.safeParse(updatePayload);
-    if (!parsedPayload.success) {
-        res.status(411).json({
-            msg: "You sent the wrong inputs",
-        })
-        return;
-    }
+  await todo.update(
+    {_id: req.body.id},
+    {completed: true}
+  );
 
-    await todo.update({
-        _id: req.body.id
-    }, {
-      completed: true  
-    })
-
-    res.json({
-        msg: "Todo marked as completed"
-    })
-})
-
-app.listen(3000);
+  res.json({
+    msg: "Todo marked as completed",
+  });
+});
