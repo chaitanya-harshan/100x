1087eaab3d91df7f5ee80464422032457b237428
chaitanya <chaitanyaharshan.official@gmail.com>
2025-01-24 01:20:48 +0530

    code

--- week-8/paytm/backend/db.js
+++ week-8/paytm/backend/db.js
@@ -4,10 +4,22 @@ import { MONGODB_PAYTM_KEY } from "./config";
 mongoose.connect(MONGODB_PAYTM_KEY)
 
 const userSchema = mongoose.Schema({
-    username: String, 
+    username: {
+        type: String,
+        lowercase: true, 
+        trim: true,
+        unique: true, 
+        required: true 
+    }, 
     firstName: String,
     lastName: String,
     password: String
 })
 
+const accountSchema = mongoose.Schema({
+    userId: {type: Schema.types.ObjectId, ref: "users"},
+    balance: {type: Number, default: 10}
+})
+
 export const User = mongoose.model("users", userSchema)
+export const Account = mongoose.model("accounts", accountSchema)
